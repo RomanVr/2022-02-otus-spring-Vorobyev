c3ee964a7926342fb6f1e315314c4860172fba11
RomanVr <romakf99@yandex.ru>
2022-03-18 18:47:11 +0300

    add load file from test properties

--- studenttest/src/test/java/ru/homework/parse/ParseCsvTest.java
+++ studenttest/src/test/java/ru/homework/parse/ParseCsvTest.java
@@ -1,35 +1,51 @@
 package ru.homework.parse;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+//@TestPropertySource(properties = {"pathfile=file.csv", "delimiter=,"})
+@TestPropertySource("classpath:application–test.properties")
+@ExtendWith(SpringExtension.class)
 @DisplayName("Класс чтения csv")
 class ParseCsvTest {
+    @Value("${delimiter}")
+    private String delimiter;
 
-    private static final String DELIMITER = ",";
-    private static final String FILENAME = "file.csv";
+    @Value("${pathfile}")
+    private String filename;
+
     private ParseCsvImpl parseCsv;
+
     @BeforeEach
-    void init() {
-        parseCsv = new ParseCsvImpl(DELIMITER, FILENAME);
+    void setUp() {
+        parseCsv = new ParseCsvImpl(delimiter, filename);
     }
 
     @DisplayName("Должен создаваться разделитель")
     @Test
     void getDelimiter() {
-        assertEquals(DELIMITER, parseCsv.getDelimiter());
+        assertEquals(delimiter, parseCsv.getDelimiter());
     }
+
     @DisplayName("Должно создаваться имя файла")
     @Test
     void getFileNameQuestion() {
-        assertEquals(FILENAME, parseCsv.getFileNameQuestion());
+        assertEquals(filename, parseCsv.getFileNameQuestion());
     }
 
-    @Disabled
     @Test
     void getDataFromCsv() {
+        List<List<String>> data = parseCsv.getDataFromCsv();
+        int size = 2;
+        assertEquals(size, data.size());
     }
 }
