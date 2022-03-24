face63c39ad4ce9fd0c002fcb2010203a38f10aa
r.vorobyev <romakf99@yandex.ru>
2022-03-24 16:46:37 +0300

    refactor pom

--- studenttest/src/main/java/ru/homework/Main.java
+++ studenttest/src/main/java/ru/homework/Main.java
@@ -1,17 +1,15 @@
 package ru.homework;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import ru.homework.output.TakeExam;
 import ru.homework.output.TakeExamInConsole;
 
-@PropertySource(value = "classpath:application.properties", encoding = "UTF-8")
-@ComponentScan
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+        ApplicationContext context = SpringApplication.run(Main.class);
         TakeExam takeExam = context.getBean(TakeExamInConsole.class);
         takeExam.runExam();
     }
