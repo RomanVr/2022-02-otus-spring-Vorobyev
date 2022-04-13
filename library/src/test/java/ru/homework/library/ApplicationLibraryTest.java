5c4e5b332e9c9465be49a9a3fc2014f0b0ceed4d
r.vorobyev <romakf99@yandex.ru>
2022-04-13 12:58:21 +0300

    init task, description

---
+++ library/src/test/java/ru/homework/library/ApplicationLibraryTest.java
@@ -0,0 +1,12 @@
+package ru.homework.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationLibraryTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
