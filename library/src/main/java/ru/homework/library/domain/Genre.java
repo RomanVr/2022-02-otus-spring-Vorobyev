e0b941f9bd3d152eb2ee61c50ef36c05f7e48898
r.vorobyev <romakf99@yandex.ru>
2022-04-19 15:31:03 +0300

    added genre tests

--- library/src/main/java/ru/homework/library/domain/Genre.java
+++ library/src/main/java/ru/homework/library/domain/Genre.java
@@ -34,4 +34,46 @@ public class Genre {
         return genreBuilder.toString();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        } else if (!(o instanceof Genre)) {
+            return false;
+        } else {
+            Genre other = (Genre) o;
+            if (!other.canEqual(this)) {
+                return false;
+            } else {
+                Object this$genreTitle = this.getGenreTitle();
+                Object other$genreTitle = other.getGenreTitle();
+                if (this$genreTitle == null) {
+                    if (other$genreTitle != null) {
+                        return false;
+                    }
+                } else if (!this$genreTitle.equals(other$genreTitle)) {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+
+    protected boolean canEqual(Object other) {
+        return other instanceof Genre;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        long $id = this.getId();
+        result = result * 59 + (int) ($id >>> 32 ^ $id);
+        Object $genreTitle = this.getGenreTitle();
+        result = result * 59 + ($genreTitle == null ? 43 : $genreTitle.hashCode());
+        Object $bookList = this.getBookList();
+        result = result * 59 + ($bookList == null ? 43 : $bookList.hashCode());
+        return result;
+    }
+
 }
---
+++ library/src/test/java/ru/homework/library/dao/GenreDaoJdbcTest.java
@@ -0,0 +1,78 @@
+package ru.homework.library.dao;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.homework.library.domain.Genre;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+@Import(GenreDaoJdbc.class)
+@DisplayName("Dao Жанров")
+class GenreDaoJdbcTest {
+
+    private static final int EXPECTED_COUNT_GENRES = 1;
+    private static final String EXPECTED_TITLE_GENRE = "Java book";
+    private static final int EXPECTED_ID_GENRE = 1;
+    @Autowired
+    private GenreDao genreDao;
+
+    @Test
+    @DisplayName("Должно получать Жанр по id")
+    void getById() {
+        var expectedGenre = new Genre(EXPECTED_ID_GENRE, EXPECTED_TITLE_GENRE);
+        var actualGenre = genreDao.getById(EXPECTED_ID_GENRE);
+        assertThat(expectedGenre).isEqualTo(actualGenre);
+    }
+
+    @Test
+    @DisplayName("Должно получать Жанр по названию")
+    void getByTitle() {
+        var expectedGenre = new Genre(EXPECTED_TITLE_GENRE);
+        var actualGenre = genreDao.getByTitle(EXPECTED_TITLE_GENRE);
+        assertThat(expectedGenre).isEqualTo(actualGenre);
+    }
+
+    @Test
+    @DisplayName("Должно добавлять Жанр в БД")
+    void shouldAddGenreToDB() {
+        var expectedGenre = new Genre("genreTest");
+        var insertId = genreDao.insert(expectedGenre);
+        var actualGenre = genreDao.getById(insertId);
+        assertThat(actualGenre).isEqualTo(expectedGenre);
+    }
+
+    @Test
+    @DisplayName("Должно обновлять Жанр")
+    void shouldUpdateGenre() {
+        var expectedGenre = new Genre(1, "genreTest");
+        genreDao.update(expectedGenre);
+        var actualGenre = genreDao.getById(expectedGenre.getId());
+        assertThat(actualGenre).isEqualTo(expectedGenre);
+    }
+
+    @Test
+    @DisplayName("Должно удалять жанр по id")
+    void shouldDeleteGenre() {
+        var expectedGenre = new Genre("genreTest");
+        var insertId = genreDao.insert(expectedGenre);
+        assertThatCode(() -> genreDao.getById(insertId)).doesNotThrowAnyException();
+
+        genreDao.deleteById(insertId);
+
+        assertThat(genreDao.getById(insertId)).isNull();
+    }
+
+    @Test
+    @DisplayName("Должно возвращать все Жанры")
+    void shouldGetAllGenres() {
+        List<Genre> actualGenreList = genreDao.getAll();
+        assertThat(actualGenreList.size()).isEqualTo(EXPECTED_COUNT_GENRES);
+    }
+}
